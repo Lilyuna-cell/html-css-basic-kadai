0350a7cabf7ce3a41f17677f32fa0d9144724440
Lilyuna-cell <papyui0505@icloud.com>
2025-06-23 17:35:00 +0900

    add 2file

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+//1以上の数字
+let num =7;
+console.log(num);
+
+//変数(num)が３と５の倍数の場合：”３と5の倍数です”
+if (num % 3 === 0 && num % 5 ===0) {
+  console.log('3と5の倍数です');
+}
+//変数(num)が３の倍数の場合：”３の倍数です”
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+
+//変数(num)が５の倍数の場合：”５の倍数です”
+else if (num % 5 ===0) {
+  console.log('5の倍数です');
+}

3cb340e677aae13b4bfce2b7935afff99bff46c5
Lilyuna-cell <papyui0505@icloud.com>
2025-06-29 18:11:55 +0900

    new one

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
-const date = new Date(2024, 10, 12);
+const date = new Date(2024, 9, 12);
 const year = date.getFullYear();
-const month = date.getMonth();
+const month = date.getMonth() +1;
 const day = date.getDate();
 
 console.log (year +'年' + month +'月' + day +'日');
